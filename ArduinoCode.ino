c9560cfcc714f83dcd4618daf01ee21f0dc019fa
Reshma S <106852906+ReshmaS21@users.noreply.github.com>
2023-04-27 04:42:36 +0530

    Update ArduinoCode.ino

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -1,4 +1,3 @@
-int flag = 0;
 
 void setup()
 
@@ -9,9 +8,6 @@ void setup()
   pinMode(12, INPUT);     //flame sensor       
 
   Serial.begin(9600);    // the GPRS baud rate
-
-  Module_Init();
-
 }
 
 void loop()
@@ -19,32 +15,18 @@ void loop()
 {
 
   int fire = digitalRead(12);
-
-  if (fire == 0)
-
+  if (fire != 0)
   {
 
     digitalWrite(9, HIGH);
 
-    delay(6000);
-
-    flag = 0;
+    delay(3000);
 
   }
-
   else
-
   {
 
     digitalWrite(9, LOW);
-
-    if (flag == 0)
-
-    {
-
-      flag = 1;
-
-    }
 
   }
 
